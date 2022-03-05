167b00373fdf075e5ba8f953317986c61cea06c5
zxm14 <91741151+zxm14@users.noreply.github.com>
2022-03-05 19:48:16 +0800

    Delete 第九章 directory

--- 第九章/1.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#define DAYS_FEB(year) year%400==0||year%4==0&&year%100!=0?29:28
-int main()
-{
-	int y;
-	scanf("%d",&y); 
-	printf("days of the FEB.: %d\n",DAYS_FEB(y));
-	return 0;
-}
